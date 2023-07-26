7a38780d72a92518ee00cc87a3c2d728a9bbb8c1
drteeth <drteeth339@gmail.com>
2023-07-26 12:37:32 -0400

    added include statements

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,40 +4,46 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  try{
-    const categoriesData = await Category.findAll();
-  console.log(categoriesData)
-    return res.json(categoriesData);}
-    catch(err){
-      console.log(err)
-      return res.json(err)
-    }
-  });
+  try {
+    const categoriesData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    console.log(categoriesData)
+    return res.json(categoriesData);
+  }
+  catch (err) {
+    console.log(err)
+    return res.json(err)
+  }
+});
 
 router.get('/:id', async (req, res) => {
-  try{
-  const categoryData = await Category.findOne(
-    {
+  try {
+    const categoryData = await Category.findOne({
+      include: {  model: Product,
       where: {
-        id: req.params.id
-      }
-    }
-  ).then((categoryData) => {
-    console.log(categoryData);
-   return res.json(categoryData)});}
-   catch(err){
-      console.log(err)
-      return res.json(err)
-    }
-  });
+          id: req.params.id,
+        }
+      }}
+
+    ).then((categoryData) => {
+      console.log(categoryData);
+      return res.json(categoryData)
+    });
+  }
+  catch (err) {
+    console.log(err)
+    return res.json(err)
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
     const categoryData = await Category.create(req.body);
-  
+
     res.status(200).json(categoryData);
   } catch (err) {
-  
+
     res.status(400).json(err);
   }
 });
